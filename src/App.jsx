1b3f2e7abf750ca9e74f5a750d506fd1c4f60c35
Naomi-Faffy <faffynaomi@gmail.com>
2025-11-05 17:38:30 +0200

    Update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './styles.css';
 import { useExquisiteCarsInteractions } from './script.js';
 import Gallery from './pages/Gallery';
 import Services from './pages/Services';
